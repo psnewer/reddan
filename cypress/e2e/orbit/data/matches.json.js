d6d5383a18762b943c6ebc3bc6cd8d1437bb4184
miller <psnewer@163.com>
2023-11-16 14:19:05 +0800

    none

---
+++ cypress/e2e/orbit/data/matches.json.js
@@ -0,0 +1,5 @@
+export const matches = [
+    {"sport":"Tennis", "competition": "UTR United Kingdom Ladies II 2023", "home":"J Rennert", "away":"S Anane", "market":"Match Odds", "runner":"J Rennert", "strategy":{"name":"tennis_1","params":{'winSet':{'set':1,'oth':true,'side':'BACK','price':100,'scale':0.5}}}},
+]
+
+
--- cypress/e2e/orbit/orbit.cy.js
+++ cypress/e2e/orbit/orbit.cy.js
@@ -7,67 +7,67 @@ describe('Login to www.orbitxch.com', function() {
 
   it(`Navigate and retrieve data for ${matches.match}`, () => {
 
-  //   cy.visit('www.orbitxch.com'); // 起始 URL
+    cy.visit('www.orbitxch.com'); // 起始 URL
 
-  //   //遍历每场比赛，找到比赛ID，供直接request请求比赛数据使用。
-  //   const arry = [];
-  //   matches.forEach(match => {
-  //     let matchItem = match;
-  //   // 点击 match.sport
-  //     cy.contains(match.sport,{timeout: 30000}).click();
+    //遍历每场比赛，找到比赛ID，供直接request请求比赛数据使用。
+    const arry = [];
+    matches.forEach(match => {
+      let matchItem = match;
+    // 点击 match.sport
+      cy.contains(match.sport,{timeout: 30000}).click();
 
-  //   // 点击 match.competition
-  //     cy.contains(match.competition,{timeout: 20000}).click();
+    // 点击 match.competition
+      cy.contains(match.competition,{timeout: 20000}).click();
 
-  //   // 点击同时含有 team1 和 team2 的组件
-  //     cy.get(`:has(p:contains(${match.home})):has(p:contains(${match.away}))`,{timeout:30000}).as('targetMatch')
-  //       .closest('div[data-event-id]')
-  //       .invoke('attr', 'data-event-id')
-  //       .then(eventId => {
-  //         matchItem['data-event-id'] = eventId;
-  //       });
-  //     cy.get('@targetMatch',{timeout: 30000}).closest('div.biab_market-title-cell').click()
+    // 点击同时含有 team1 和 team2 的组件
+      cy.get(`:has(p:contains(${match.home})):has(p:contains(${match.away}))`,{timeout:30000}).as('targetMatch')
+        .closest('div[data-event-id]')
+        .invoke('attr', 'data-event-id')
+        .then(eventId => {
+          matchItem['data-event-id'] = eventId;
+        });
+      cy.get('@targetMatch',{timeout: 30000}).closest('div.biab_market-title-cell').click()
     
-  //     matchItem['oth_runner'] = getOth(match.home, match.away, match.runner);
-  //     matchItem['handicap'] = Number(getHandicap(match.runner,match.home,match.away))
-  //     matchItem['oth_handicap'] = Number(getHandicap(match.oth_runner,match.home,match.away))
+      matchItem['oth_runner'] = getOth(match.home, match.away, match.runner);
+      matchItem['handicap'] = Number(getHandicap(match.runner,match.home,match.away))
+      matchItem['oth_handicap'] = Number(getHandicap(match.oth_runner,match.home,match.away))
 
-  //   // 点击 match.market
-  //     cy.contains(match.market,{timeout: 40000}).closest('[data-sport-id]')
-  //       .invoke('attr', 'data-sport-id')
-  //       .then(sportId => {
-  //         matchItem['data-market-id'] = sportId;
-  //         cy.contains(match.market).click()
-  //         cy.url().should('include',sportId).then(runner_url => {
-  //         matchItem['runner_url'] = runner_url;
-  //         //获取selectionId
-  //         cy.get('body').then($body => {
-  //           if ($body.find('span:contains("Show all")').length) {
-  //             // 如果找到了包含 'Show all' 的 span，则点击
-  //             cy.contains('span', 'Show all').click();
-  //           } 
-  //         }).then(() => {
-  //           let runnerEscaped = matchItem['runner'].replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-  //           cy.contains('span', new RegExp(`^${runnerEscaped}$`)).closest('div[data-market-id]')
-  //             .find('[data-selection-id]').first()
-  //             .invoke('attr', 'data-selection-id')
-  //             .then(dataSelectionId => {
-  //               matchItem['selectionId'] = dataSelectionId;
-  //             });
-  //           let oth_runnerEscaped = matchItem['oth_runner'].replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-  //           cy.contains('span', new RegExp(`^${oth_runnerEscaped}$`)).closest('div[data-market-id]')
-  //             .find('[data-selection-id]').first()
-  //             .invoke('attr', 'data-selection-id')
-  //             .then(dataSelectionId => {
-  //               matchItem['oth_selectionId'] = dataSelectionId;
-  //             });
-  //         });
-  //     })
-  //     });
-  //     arry.push(matchItem);
-  // });
+    // 点击 match.market
+      cy.contains(match.market,{timeout: 40000}).closest('[data-sport-id]')
+        .invoke('attr', 'data-sport-id')
+        .then(sportId => {
+          matchItem['data-market-id'] = sportId;
+          cy.contains(match.market).click()
+          cy.url().should('include',sportId).then(runner_url => {
+          matchItem['runner_url'] = runner_url;
+          //获取selectionId
+          cy.get('body').then($body => {
+            if ($body.find('span:contains("Show all")').length) {
+              // 如果找到了包含 'Show all' 的 span，则点击
+              cy.contains('span', 'Show all').click();
+            } 
+          }).then(() => {
+            let runnerEscaped = matchItem['runner'].replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+            cy.contains('span', new RegExp(`^${runnerEscaped}$`)).closest('div[data-market-id]')
+              .find('[data-selection-id]').first()
+              .invoke('attr', 'data-selection-id')
+              .then(dataSelectionId => {
+                matchItem['selectionId'] = dataSelectionId;
+              });
+            let oth_runnerEscaped = matchItem['oth_runner'].replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+            cy.contains('span', new RegExp(`^${oth_runnerEscaped}$`)).closest('div[data-market-id]')
+              .find('[data-selection-id]').first()
+              .invoke('attr', 'data-selection-id')
+              .then(dataSelectionId => {
+                matchItem['oth_selectionId'] = dataSelectionId;
+              });
+          });
+      })
+      });
+      arry.push(matchItem);
+  });
 
-  // cy.task('saveToFile', arry);
+  cy.task('saveToFile', arry);
 
 });
 })

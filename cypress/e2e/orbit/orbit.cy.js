e447a66e273a1714df95fc45e7498af516708d87
miller <psnewer@163.com>
2023-11-16 14:22:30 +0800

    none

--- cypress/e2e/orbit/orbit.cy.js
+++ cypress/e2e/orbit/orbit.cy.js
@@ -7,7 +7,7 @@ describe('Login to www.orbitxch.com', function() {
 
   it(`Navigate and retrieve data for ${matches.match}`, () => {
 
-    cy.visit('www.orbitxch.com'); // 起始 URL
+    cy.visit('https://www.orbitxch.com'); // 起始 URL
 
     //遍历每场比赛，找到比赛ID，供直接request请求比赛数据使用。
     const arry = [];
@@ -72,59 +72,59 @@ describe('Login to www.orbitxch.com', function() {
 });
 })
 
-describe('Execution after login', function() {
+// describe('Execution after login', function() {
 
-  // Cypress 默认在每个测试用例之前会清除 cookies，要阻止这一行为，我们可以使用以下代码
-  beforeEach(() => {
-    cy.session('loggedInSession', () => {
-        cy.login('abc', 'abc');
-    });
-  });
+//   // Cypress 默认在每个测试用例之前会清除 cookies，要阻止这一行为，我们可以使用以下代码
+//   beforeEach(() => {
+//     cy.session('loggedInSession', () => {
+//         cy.login('abc', 'abc');
+//     });
+//   });
 
-  for (let i = 0; i < 2; i++) {
-it(`Navigate match events and place bets`, () => {
+//   for (let i = 0; i < 2; i++) {
+// it(`Navigate match events and place bets`, () => {
 
-const executor = new StrategyExecutor('./data/strategy.json');
+// const executor = new StrategyExecutor('./data/strategy.json');
 
-cy.visit('http://www.orbitxch.com',{timeout:20000})
+// cy.visit('http://www.orbitxch.com',{timeout:20000})
 
-function setupInterception() {
+// function setupInterception() {
 
-  cy.wait(5000).then(() => {
-  //获取currentBets
-  cy.intercept({
-    hostname : 'www.orbitxch.com',
-    pathname : "/customer/api/currentBets"
-  }).as('currentBets')
+//   cy.wait(5000).then(() => {
+//   //获取currentBets
+//   cy.intercept({
+//     hostname : 'www.orbitxch.com',
+//     pathname : "/customer/api/currentBets"
+//   }).as('currentBets')
 
-  cy.wait('@currentBets',{timeout:60000}).then( res => {
+//   cy.wait('@currentBets',{timeout:60000}).then( res => {
         
-    cy.task('readJsonFile','cypress/e2e/orbit/data/bets.json').then(betIds => {
-      betIds.forEach(bet => {
-        try{
-              bet.currentBets = res.response.body;
-              cy.getEventData(bet).then(params => {
-                cy.log(params)
-                cy.executeStrategy(executor,bet.strategy.name, params)
-                  .then(undefined, (error) => {
-                                              console.error(error);
-                    });
-               }).then(undefined, (error) => {
-                    console.error(error);
-                  });
-        }catch {
-                (error) => {
-                  console.error(error);
-                }
-          }
-      }); 
-    })
-  });
-})
-}
+//     cy.task('readJsonFile','cypress/e2e/orbit/data/bets.json').then(betIds => {
+//       betIds.forEach(bet => {
+//         try{
+//               bet.currentBets = res.response.body;
+//               cy.getEventData(bet).then(params => {
+//                 cy.log(params)
+//                 cy.executeStrategy(executor,bet.strategy.name, params)
+//                   .then(undefined, (error) => {
+//                                               console.error(error);
+//                     });
+//                }).then(undefined, (error) => {
+//                     console.error(error);
+//                   });
+//         }catch {
+//                 (error) => {
+//                   console.error(error);
+//                 }
+//           }
+//       }); 
+//     })
+//   });
+// })
+// }
 
-setupInterception();
+// setupInterception();
 
-});
-  }
-})
+// });
+//   }
+// })

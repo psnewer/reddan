3b193d91f38181bdc4b904cf57c322cbc24b39af
miller <psnewer@163.com>
2023-11-21 22:53:57 +0800

    none

--- cypress/e2e/orbit/data/matches.json.js
+++ cypress/e2e/orbit/data/matches.json.js
@@ -1,5 +1,5 @@
 export const matches = [
-    {"sport":"Soccer", "competition": "International", "home":"Ecuador", "away":"Chile", "market":"Asian Handicap", "runner":"Chile +0.5", "strategy":{"name":"soccer_1","params":{'notInPlay':{'side':'BACK','vol':6,'price':2.9},'notMatchOne':{'side':'BACK','vol':6,'cut_time':5},'notPlaceTwo':{'oth':true,'side':'BACK','profit':0.3,'scale':1.0},'isRunnerAdvance':{'side':'LAY','scale':1.0},'timeElapseTo':{'oth':true,'time_to':45,'side':'BACK','scale':1.0}}}},
+    {"sport":"Soccer", "competition": "English Premier League", "home":"Man City", "away":"Liverpool", "market":"Asian Handicap", "runner":"Liverpool +0.5", "strategy":{"name":"soccer_1","params":{'notInPlay':{'side':'BACK','vol':6,'price':2.5},'notMatchOne':{'side':'BACK','vol':6,'cut_time':5},'notPlaceTwo':{'oth':true,'side':'BACK','profit':0.3,'scale':1.0},'isRunnerAdvance':{'side':'LAY','scale':1.0},'timeElapseTo':{'oth':true,'time_to':45,'side':'BACK','scale':1.0}}}},
 ]
 
 

d913ceb0d7056e8bd3722fd67fc26c39283156ca
miller <psnewer@163.com>
2023-11-23 15:41:51 +0800

    none

--- run_orbit.sh
+++ run_orbit.sh
@@ -5,12 +5,24 @@ timestamp() {
   date +"%Y-%m-%d %H:%M:%S"
 }
 
+check_command() {
+    pgrep -f "cypress run --spec cypress/e2e/orbit/iter.cy.js" > /dev/null
+    return $?
+}
+
+check_crash() {
+    grep -q "crashed" ./iter.log > /dev/null
+    return $?
+}
+
 # 检查是否存在正在运行的 Cypress 命令
-if ! pgrep -f "cypress run --spec cypress/e2e/orbit/iter.cy.js"; then
-    echo "Cypress 命令未在运行，启动命令..."
-	echo "$(timestamp): 开始执行 Cypress 测试"
-    npx cypress run --spec cypress/e2e/orbit/iter.cy.js
-else
-    echo "Cypress 命令已在运行。"
+if ! check_command; then
+	echo "$(timestamp): Cypress 命令未在运行，启动命令..."
+    npx cypress run --spec cypress/e2e/orbit/iter.cy.js > iter.log 2>&1
+fi
+
+if check_crash; then
+    echo "$(timestamp): 检测到异常状态，尝试杀死进程"
+    pkill -f "cypress run --spec cypress/e2e/orbit/iter.cy.js"
 fi
 

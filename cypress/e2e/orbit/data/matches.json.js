bd897e56d1edc737dfd4378d7d023945c3673400
miller <psnewer@163.com>
2024-01-19 20:00:06 +0800

    none

--- cypress/e2e/orbit/data/matches.json.js
+++ cypress/e2e/orbit/data/matches.json.js
@@ -1,13 +1,119 @@
 export const matches = [
-    {"sport":"Tennis", "competition": "Australian Open 2024", "home":"Adrian Mannarino", "away":"Ben Shelton", "market":"Match Odds", "runner":"Adrian Mannarino", "strategy":{"name":"tennis_20","params": {
+    {"sport":"Tennis", "competition": "Australian Open 2024", "home":"Juncheng Shang", "away":"Carlos Alcaraz", "market":"Match Odds", "runner":"Carlos Alcaraz", "strategy":{"name":"tennis_1","params": {
+        "notMatchOne": {
+            "side": "BACK",
+            "vol": 10,
+            "delta": 2,
+            "set": 1,
+            "price": 1.62
+        },
+        "notMatchTwo": {
+            "side": "LAY",
+            "scale": 1.0
+        }
+    }}},
+    {"sport":"Tennis", "competition": "Australian Open 2024", "home":"Alexander Zverev", "away":"Alex Michelsen", "market":"Match Odds", "runner":"Alexander Zverev", "strategy":{"name":"tennis_1","params": {
+        "notMatchOne": {
+            "side": "BACK",
+            "vol": 10,
+            "delta": 2,
+            "set": 1,
+            "price": 1.62
+        },
+        "notMatchTwo": {
+            "side": "BACK",
+            "scale": 1.0
+        }
+    }}},
+    {"sport":"Tennis", "competition": "Australian Open 2024", "home":"Felix Auger-Aliassime", "away":"Daniil Medvedev", "market":"Match Odds", "runner":"Daniil Medvedev", "strategy":{"name":"tennis_1","params": {
+        "notMatchOne": {
+            "side": "BACK",
+            "vol": 10,
+            "delta": 2,
+            "set": 1,
+            "price": 1.62
+        },
+        "notMatchTwo": {
+            "side": "LAY",
+            "scale": 1.0
+        }
+    }}},
+    {"sport":"Tennis", "competition": "Australian Open 2024", "home":"Ugo Humbert", "away":"Hubert Hurkacz", "market":"Match Odds", "runner":"Ugo Humbert", "strategy":{"name":"tennis_21","params": {
+        "notMatchOne": {
+            "side": "BACK",
+            "vol": 10,
+            "delta": 3
+        },
+        "loseSet": {
+            "set": 3,
+            "side": "BACK",
+            "scale": 1.0,
+            "rec": 0.3
+        },
+        "notMatchTwo": {
+            "side": "BACK",
+            "scale": 1.0
+        }
+    }}},
+    {"sport":"Tennis", "competition": "Australian Open 2024", "home":"Cameron Norrie", "away":"Casper Ruud", "market":"Match Odds", "runner":"Casper Ruud", "strategy":{"name":"tennis_22","params": {
+        "notMatchOne": {
+            "side": "BACK",
+            "vol": 10,
+            "delta": 3
+        },
+        "eitherLose": {
+            "set": 3,
+            "side": "BACK",
+            "scale": 1.0,
+            "rec": 0.3
+        },
+        "notMatchTwo": {
+            "side": "BACK",
+            "scale": 1.0
+        }
+    }}},
+    {"sport":"Tennis", "competition": "Australian Open 2024", "home":"Tommy Paul", "away":"Miomir Kecmanovic", "market":"Match Odds", "runner":"Miomir Kecmanovic", "strategy":{"name":"tennis_21","params": {
+        "notMatchOne": {
+            "side": "BACK",
+            "vol": 10,
+            "delta": 3
+        },
+        "loseSet": {
+            "set": 3,
+            "side": "BACK",
+            "scale": 1.0,
+            "rec": 0.3
+        },
+        "notMatchTwo": {
+            "side": "BACK",
+            "scale": 1.0
+        }
+    }}},
+    {"sport":"Tennis", "competition": "Australian Open 2024", "home":"Grigor Dimitrov", "away":"Nuno Borges", "market":"Match Odds", "runner":"Nuno Borges", "strategy":{"name":"tennis_21","params": {
         "notMatchOne": {
             "side": "BACK",
             "vol": 10,
             "delta": 3,
-            "set": 1,
             "price": 1.62
         },
         "loseSet": {
+            "set": 3,
+            "side": "BACK",
+            "scale": 1.0,
+            "rec": 0.3
+        },
+        "notMatchTwo": {
+            "side": "BACK",
+            "scale": 1.0
+        }
+    }}},
+    {"sport":"Tennis", "competition": "Australian Open 2024", "home":"Arthur Cazaux", "away":"Tallon Griekspoor", "market":"Match Odds", "runner":"Tallon Griekspoor", "strategy":{"name":"tennis_22","params": {
+        "notMatchOne": {
+            "side": "BACK",
+            "vol": 10,
+            "delta": 3
+        },
+        "eitherLose": {
             "set": 3,
             "side": "BACK",
             "scale": 1.0,

b17d22cc4266d42e3e50716aba16781826df1339
miller <psnewer@163.com>
2024-01-20 19:56:42 +0800

    none

--- cypress/e2e/orbit/data/matches.json.js
+++ cypress/e2e/orbit/data/matches.json.js
@@ -1,124 +1,63 @@
 export const matches = [
-    {"sport":"Tennis", "competition": "Australian Open 2024", "home":"Juncheng Shang", "away":"Carlos Alcaraz", "market":"Match Odds", "runner":"Carlos Alcaraz", "strategy":{"name":"tennis_1","params": {
+    {"sport":"Soccer", "competition": "German Bundesliga", "home":"Freiburg", "away":"Hoffenheim", "market":"Asian Handicap", "runner":"Freiburg +0.5", "strategy":{"name":"soccer_1","params": {
         "notMatchOne": {
             "side": "BACK",
             "vol": 10,
-            "delta": 2,
-            "set": 1,
-            "price": 1.62
+            'time_to': 60
         },
         "notMatchTwo": {
             "side": "LAY",
-            "scale": 1.0
+            "scale": 1.0,
+            "time_to": 85
         }
     }}},
-    {"sport":"Tennis", "competition": "Australian Open 2024", "home":"Alexander Zverev", "away":"Alex Michelsen", "market":"Match Odds", "runner":"Alexander Zverev", "strategy":{"name":"tennis_1","params": {
+    {"sport":"Soccer", "competition": "German Bundesliga", "home":"FC Heidenheim", "away":"Wolfsburg", "market":"Asian Handicap", "runner":"FC Heidenheim +0.5", "strategy":{"name":"soccer_1","params": {
         "notMatchOne": {
             "side": "BACK",
             "vol": 10,
-            "delta": 2,
-            "set": 1,
-            "price": 1.62
-        },
-        "notMatchTwo": {
-            "side": "BACK",
-            "scale": 1.0
-        }
-    }}},
-    {"sport":"Tennis", "competition": "Australian Open 2024", "home":"Felix Auger-Aliassime", "away":"Daniil Medvedev", "market":"Match Odds", "runner":"Daniil Medvedev", "strategy":{"name":"tennis_1","params": {
-        "notMatchOne": {
-            "side": "BACK",
-            "vol": 10,
-            "delta": 2,
-            "set": 1,
-            "price": 1.62
+            'time_to': 60
         },
         "notMatchTwo": {
             "side": "LAY",
-            "scale": 1.0
+            "scale": 1.0,
+            "time_to": 85
         }
     }}},
-    {"sport":"Tennis", "competition": "Australian Open 2024", "home":"Ugo Humbert", "away":"Hubert Hurkacz", "market":"Match Odds", "runner":"Hubert Hurkacz", "strategy":{"name":"tennis_21","params": {
+    {"sport":"Soccer", "competition": "German Bundesliga", "home":"RB Leipzig", "away":"Leverkusen", "market":"Asian Handicap", "runner":"Leverkusen +0.5", "strategy":{"name":"soccer_2","params": {
         "notMatchOne": {
             "side": "BACK",
             "vol": 10,
-            "delta": 3
-        },
-        "loseSet": {
-            "set": 3,
-            "side": "BACK",
-            "scale": 1.0,
-            "rec": 0.3
+            'time_to': 60,
+            'handicap': 0.5
         },
         "notMatchTwo": {
-            "scale": 1.0
+            "side": "BACK",
+            "scale": 1.0,
+            "time_to": 85
         }
     }}},
-    {"sport":"Tennis", "competition": "Australian Open 2024", "home":"Cameron Norrie", "away":"Casper Ruud", "market":"Match Odds", "runner":"Casper Ruud", "strategy":{"name":"tennis_22","params": {
+    {"sport":"Soccer", "competition": "Spanish La Liga", "home":"Rayo Vallecano", "away":"Las Palmas", "market":"Asian Handicap", "runner":"Rayo Vallecano +0.5", "strategy":{"name":"soccer_1","params": {
         "notMatchOne": {
             "side": "BACK",
             "vol": 10,
-            "delta": 3
-        },
-        "eitherLose": {
-            "set": 3,
-            "side": "BACK",
-            "scale": 1.0,
-            "rec": 0.3
+            'time_to': 60
         },
         "notMatchTwo": {
-            "side": "BACK",
-            "scale": 1.0
+            "side": "LAY",
+            "scale": 1.0,
+            "time_to": 85
         }
     }}},
-    {"sport":"Tennis", "competition": "Australian Open 2024", "home":"Tommy Paul", "away":"Miomir Kecmanovic", "market":"Match Odds", "runner":"Tommy Paul", "strategy":{"name":"tennis_21","params": {
+    {"sport":"Soccer", "competition": "Spanish La Liga", "home":"Villarreal", "away":"Mallorca", "market":"Asian Handicap", "runner":"Villarreal +0.5", "strategy":{"name":"soccer_1","params": {
         "notMatchOne": {
             "side": "BACK",
             "vol": 10,
-            "delta": 3
-        },
-        "loseSet": {
-            "set": 3,
-            "side": "BACK",
-            "scale": 1.0,
-            "rec": 0.3
+            'time_to': 60
         },
         "notMatchTwo": {
-            "scale": 1.0
-        }
-    }}},
-    {"sport":"Tennis", "competition": "Australian Open 2024", "home":"Grigor Dimitrov", "away":"Nuno Borges", "market":"Match Odds", "runner":"Grigor Dimitrov", "strategy":{"name":"tennis_21","params": {
-        "notMatchOne": {
-            "side": "BACK",
-            "vol": 10,
-            "delta": 3,
-            "price": 1.62
-        },
-        "loseSet": {
-            "set": 3,
-            "side": "BACK",
+            "side": "LAY",
             "scale": 1.0,
-            "rec": 0.3
-        },
-        "notMatchTwo": {
-            "scale": 1.0
-        }
-    }}},
-    {"sport":"Tennis", "competition": "Australian Open 2024", "home":"Arthur Cazaux", "away":"Tallon Griekspoor", "market":"Match Odds", "runner":"Tallon Griekspoor", "strategy":{"name":"tennis_22","params": {
-        "notMatchOne": {
-            "side": "BACK",
-            "vol": 10,
-            "delta": 3
-        },
-        "eitherLose": {
-            "set": 3,
-            "side": "BACK",
-            "scale": 1.0,
-            "rec": 0.3
-        },
-        "notMatchTwo": {
-            "side": "BACK",
-            "scale": 1.0
+            "time_to": 85
         }
     }}},
 ]

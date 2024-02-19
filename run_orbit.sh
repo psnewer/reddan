0c5102d49ea72daec616edeeac73bc1b32482c04
miller <psnewer@163.com>
2024-02-19 19:47:33 +0800

    none

--- run_orbit.sh
+++ run_orbit.sh
@@ -6,25 +6,25 @@ timestamp() {
 }
 
 check_command() {
-    pgrep -f "cypress run --spec cypress/e2e/orbit/iter.cy.js" > /dev/null
+    pgrep -f "node iter.js" > /dev/null
     return $?
 }
 
 check_crash() {
-    grep -qi "error" ./iter.log > /dev/null
+    grep -qi "Error" ./iter.log > /dev/null
     return $?
 }
 
 # 检查是否存在正在运行的 Cypress 命令
 if ! check_command; then
-	echo "$(timestamp): Cypress 命令未在运行，启动命令..."
+	echo "$(timestamp): Playwright 命令未在运行，启动命令..."
     rm -f iter.log
     touch iter.log
-    npx cypress run --spec cypress/e2e/orbit/iter.cy.js > iter.log 2>&1
+    node iter.js > iter.log 2>&1
 fi
 
 if check_crash; then
     echo "$(timestamp): 检测到异常状态，尝试杀死进程"
-    pkill -f "cypress run --spec cypress/e2e/orbit/iter.cy.js"
+    pkill -f "node iter.js"
 fi
 

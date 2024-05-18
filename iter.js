365d0c65b35c277e7afaed7a32f50afd4747a33f
miller <psnewer@163.com>
2024-05-18 20:04:21 +0800

    none

--- iter.js
+++ iter.js
@@ -46,7 +46,7 @@ const util = require('util');
   });
 
   if (ret) {
-    
+
     const executor = new StrategyExecutor('./data/strategy.json');
     await executor.initialize();
 
@@ -65,51 +65,53 @@ const util = require('util');
       const betIds = JSON.parse(await fs.readFile('./cypress/e2e/orbit/data/bets.json', 'utf8'));
       const _betIds = JSON.parse(JSON.stringify(betIds));
       if (global.currentBets !== '') {
-        try {
-          const [score_tennis] = await Promise.all([
-            fetchData(event_tennis_url)
-            // fetchData(event_soccer_url),
-            // fetchData(event_basketball_url)
-          ]);
-          for (let i = 0; i < betIds.length; i++) {
-            let bet = betIds[i]
-            let _bet = _betIds[i]
-            bet.page = page;
-            bet.currentBets = global.currentBets.filter(item => item.marketId === bet['data-market-id']);
-            bet.currentBets.sort((a, b) => {
-              return a.matchedDate - b.matchedDate;
-            });
-            bet.score_tennis = score_tennis;
-            // bet.score_soccer = score_soccer;
-            // bet.score_basketball = score_basketball;
-            const params = await getEventData(bet);
 
+        const [score_tennis] = await Promise.all([
+          fetchData(event_tennis_url)
+          // fetchData(event_soccer_url),
+          // fetchData(event_basketball_url)
+        ]);
+        for (let i = 0; i < betIds.length; i++) {
+          let bet = betIds[i]
+          let _bet = _betIds[i]
+          bet.page = page;
+          bet.currentBets = global.currentBets.filter(item => item.marketId === bet['data-market-id']);
+          bet.currentBets.sort((a, b) => {
+            return a.matchedDate - b.matchedDate;
+          });
+          bet.score_tennis = score_tennis;
+          // bet.score_soccer = score_soccer;
+          // bet.score_basketball = score_basketball;
+          const params = await getEventData(bet);
+
+          try {
             if (checkBets(params))
               await executor.execute(params.bet.strategy.name, params);
-
-            if (params.event.hasOwnProperty('lastIsRunner_breakdown') && params.event.hasOwnProperty('lastSet_breakdown')) {
-              if (!bet.hasOwnProperty('pre'))
-                _bet.pre,bet.pre = {},{}
-              if (params.event.lastIsRunner_breakdown != bet.pre.lastIsRunner_breakdown || params.event.lastSet_breakdown != bet.pre.lastSet_breakdown) {
-                _bet.pre.lastIsRunner_breakdown = params.event.lastIsRunner_breakdown
-                _bet.pre.lastSet_breakdown = params.event.lastSet_breakdown
-                await fs.writeFile('./cypress/e2e/orbit/data/bets.json', JSON.stringify(_betIds, null, 2), 'utf8')
-              }
-            }
-
-
+          } catch (error) {
+            const subject = 'Test Failure';
+            const text = `A test has failed: Navigate match events and place bets`;
+            const errorDetails = error; // 获取错误的堆栈信息
+            const html = `
+              <p>A test has failed: <strong>Navigate match events and place bets</strong></p>
+              <p>Error details:</p>
+              <pre>${errorDetails}</pre>
+            `;
+            await sendEmail({ subject: subject, text: text, html: html });
           }
-        } catch (error) {
-          const subject = 'Test Failure';
-          const text = `A test has failed: Navigate match events and place bets`;
-          const errorDetails = error; // 获取错误的堆栈信息
-          const html = `
-          <p>A test has failed: <strong>Navigate match events and place bets</strong></p>
-          <p>Error details:</p>
-          <pre>${errorDetails}</pre>
-        `;
-          await sendEmail({ subject: subject, text: text, html: html });
+
+          if (params.event.hasOwnProperty('lastIsRunner_breakdown') && params.event.hasOwnProperty('lastSet_breakdown')) {
+            if (!bet.hasOwnProperty('pre'))
+              _bet.pre, bet.pre = {}, {}
+            if (params.event.lastIsRunner_breakdown != bet.pre.lastIsRunner_breakdown || params.event.lastSet_breakdown != bet.pre.lastSet_breakdown) {
+              _bet.pre.lastIsRunner_breakdown = params.event.lastIsRunner_breakdown
+              _bet.pre.lastSet_breakdown = params.event.lastSet_breakdown
+              await fs.writeFile('./cypress/e2e/orbit/data/bets.json', JSON.stringify(_betIds, null, 2), 'utf8')
+            }
+          }
+
+
         }
+
       }
     }
   }

2869369d82bcfe5b7efe7024dca3e38fff521fb9
miller <psnewer@163.com>
2024-05-27 19:00:24 +0800

    none

--- strategy.js
+++ strategy.js
@@ -706,23 +706,23 @@ class StrategyExecutor {
             if (params.bet.strategy.params[condition].side === 'BACK') {
                 if (!params.event.oth_back_odds || (currentBets.length && (!oth_thresh_back_odds || params.event.oth_back_odds < oth_thresh_back_odds)))
                     return
-                current_odds = params.event.oth_back_odds * 0.9 > 1.0 ? params.event.oth_back_odds * 0.9 : params.event.oth_back_odds
+                current_odds = params.event.oth_back_odds
             }
             else {
                 if (!params.event.oth_lay_odds || (currentBets.length && (!oth_thresh_lay_odds || params.event.oth_lay_odds > oth_thresh_lay_odds)))
                     return
-                current_odds = params.event.oth_lay_odds * 1.1
+                current_odds = params.event.oth_lay_odds
             }
         } else {
             if (params.bet.strategy.params[condition].side === 'BACK') {
                 if (!params.event.back_odds || (currentBets.length && (!runner_thresh_back_odds || params.event.back_odds < runner_thresh_back_odds)))
                     return
-                current_odds = params.event.back_odds * 0.9 > 1.0 ? params.event.back_odds * 0.9 : params.event.back_odds
+                current_odds = params.event.back_odds
             }
             else {
                 if (!params.event.lay_odds || (currentBets.length && (!runner_thresh_lay_odds || params.event.lay_odds > runner_thresh_lay_odds)))
                     return
-                current_odds = params.event.lay_odds * 1.1
+                current_odds = params.event.lay_odds
             }
         }
 

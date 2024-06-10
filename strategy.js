677809696daf686d5da638d73cb48f5a6a950666
miller <psnewer@163.com>
2024-06-10 11:48:16 +0800

    none

--- strategy.js
+++ strategy.js
@@ -258,8 +258,12 @@ class StrategyExecutor {
     drawGamesNotMatch(params, condition) {
         if (params.event.hasOwnProperty('lastIsRunner') && params.bet.hasOwnProperty('pre'))
             if (params.event.lastIsRunner == params.bet.pre.lastIsRunner_breakdown)
-                if (params.event.score_home.length + 1 == params.bet.pre.lastSet_breakdown)
+                if (params.event.score_home.length + 1 == params.bet.pre.lastSet_breakdown) {
+                    if (!params.bet.strategy.params[condition].hasOwnProperty('scale'))
+                        if (params.bet.strategy.params[condition].on)
+                            params.bet.strategy.params[condition]['scale'] = 0.0
                     return true
+                }
         return false
     }
 

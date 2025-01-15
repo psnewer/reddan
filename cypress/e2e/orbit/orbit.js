4d296050f46f875fe70219511b2405495ef7f80d
miller <psnewer@163.com>
2025-01-15 23:39:18 +0800

    none

--- cypress/e2e/orbit/orbit.js
+++ cypress/e2e/orbit/orbit.js
@@ -50,63 +50,63 @@ async function runTest() {
               // Set filter based on match criteria
               if (match.team == match.home_team) {
                 if (match.filter == 'VS_TAW') {
-                  filter = filters['VS_TAW']
+                  filter = structuredClone(filters['VS_TAW'])
                   filter.runner = c_away + ' +0.5'
                   filter.oth_runner = c_home + ' -0.5'
                 }
                 else if (match.filter == 'VS_TAWDRAW') {
-                  filter = filters['VS_TAWDRAW']
+                  filter = structuredClone(filters['VS_TAWDRAW'])
                   filter.runner = c_away + ' 0'
                   filter.oth_runner = c_home + ' 0'
                 }
                 else if (match.filter == 'VS_RAW') {
-                  filter = filters['VS_TAW']
+                  filter = structuredClone(filters['VS_TAW'])
                   filter.runner = c_home + ' +0.5'
                   filter.oth_runner = c_away + ' -0.5'
                 }
                 else if (match.filter == 'VS_RAWDRAW') {
-                  filter = filters['VS_TAWDRAW']
+                  filter = structuredClone(filters['VS_TAWDRAW'])
                   filter.runner = c_home + ' 0'
                   filter.oth_runner = c_away + ' 0'
                 }
                 else if (match.filter == 'VS_TAWTAW') {
-                  filter = filters['VS_TAWTAW']
+                  filter = structuredClone(filters['VS_TAWTAW'])
                   filter.runner = c_away + ' +0.5'
                   filter.oth_runner = c_home + ' -0.5'
                 }
                 else if (match.filter == 'VS_RAWRAW') {
-                  filter = filters['VS_TAWTAW']
+                  filter = structuredClone(filters['VS_TAWTAW'])
                   filter.runner = c_home + ' +0.5'
                   filter.oth_runner = c_away + ' -0.5'
                 }
               } else {
                 if (match.filter == 'VS_TAW') {
-                  filter = filters['VS_TAW']
+                  filter = structuredClone(filters['VS_TAW'])
                   filter.runner = c_home + ' +0.5'
                   filter.oth_runner = c_away + ' -0.5'
                 }
                 else if (match.filter == 'VS_TAWDRAW') {
-                  filter = filters['VS_TAWDRAW']
+                  filter = structuredClone(filters['VS_TAWDRAW'])
                   filter.runner = c_home + ' 0'
                   filter.oth_runner = c_away + ' 0'
                 }
                 else if (match.filter == 'VS_RAW') {
-                  filter = filters['VS_TAW']
+                  filter = structuredClone(filters['VS_TAW'])
                   filter.runner = c_away + ' +0.5'
                   filter.oth_runner = c_home + ' -0.5'
                 }
                 else if (match.filter == 'VS_RAWDRAW') {
-                  filter = filters['VS_TAWDRAW']
+                  filter = structuredClone(filters['VS_TAWDRAW'])
                   filter.runner = c_away + ' 0'
                   filter.oth_runner = c_home + ' 0'
                 }
                 else if (match.filter == 'VS_TAWTAW') {
-                  filter = filters['VS_TAWTAW']
+                  filter = structuredClone(filters['VS_TAWTAW'])
                   filter.runner = c_home + ' +0.5'
                   filter.oth_runner = c_away + ' -0.5'
                 }
                 else if (match.filter == 'VS_RAWRAW') {
-                  filter = filters['VS_TAWTAW']
+                  filter = structuredClone(filters['VS_TAWTAW'])
                   filter.runner = c_away + ' +0.5'
                   filter.oth_runner = c_home + ' -0.5'
                 }

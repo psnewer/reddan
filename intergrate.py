4494d565c5808ee04cc75b37f5f40b1dae0470c1
psnewer <psnewer@163.com>
2025-01-25 12:03:52 +0800

    none

--- intergrate.py
+++ intergrate.py
@@ -7,7 +7,7 @@ with open('cypress/e2e/orbit/data/cands.json', 'r') as f1, open('cypress/e2e/orb
     j2 = json.load(f2)
 
 # 统计 j1 中每个 (home, away) 组合出现的次数
-home_away_counts = Counter((item["home"], item["away"], item["selectionId"]) for item in j1)
+#home_away_counts = Counter((item["home"], item["away"], item["selectionId"]) for item in j1)
 
 # 仅保留 j1 中那些 (home, away) 相等且 selectionId 相等的项
 processed_pairs = set()  # 用于跟踪已经处理过的 (home, away) 对
@@ -19,7 +19,6 @@ for item in j1:
     # 如果当前 (home, away) 对已经处理过，跳过
     if pair in processed_pairs:
         continue
-
     # # 处理逻辑：当 home_away_counts 为 1 或匹配的第一个 selectionId 相同时添加
     # if home_away_counts[pair] == 1 or (
     #     (first_item := next(
@@ -32,11 +31,11 @@ for item in j1:
         processed_pairs.add(pair)  # 标记为已处理
 
 # 将 j2 转换为字典形式，以便查找和替换
-j2_dict = {(item["home"], item["away"]): item for item in j2}
+j2_dict = {(item["home"], item["away"], item["selectionId"]): item for item in j2}
 
 # 替换：如果 j2 中存在相同的 home, away 值，则替换 j1 中的字典
 for i, item in enumerate(filtered_j1):
-    home_away_pair = (item["home"], item["away"])
+    home_away_pair = (item["home"], item["away"], item["selectionId"])
     if home_away_pair in j2_dict:
         filtered_j1[i] = j2_dict[home_away_pair]  # 用 j2 中的字典替换 j1 中的
 

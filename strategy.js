0ba230d4ccb0befe83885cf8e66b0f143ca844ca
miller <psnewer@163.com>
2025-05-27 14:29:29 +0800

    none

--- strategy.js
+++ strategy.js
@@ -330,6 +330,8 @@ class StrategyExecutor {
                 }
             }
             else {
+                if (!params.event.lastIsRunner && !params.bet.strategy.params[condition].hasOwnProperty('side'))
+                    return true
                 if (params.event.score_home.length <= params.bet.strategy.params[condition].until) {
                     if (!params.event.lastIsRunner) {
                         if (params.event.score_home[params.event.score_home.length - 1] > params.event.score_away[params.event.score_away.length - 1] && params.bet.away == params.bet.runner)

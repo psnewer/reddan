ce56bdf384055389a370079a2b1a576e5fda8696
miller <psnewer@163.com>
2025-06-19 15:50:31 +0800

    add tennis_002

--- cypress/e2e/orbit/get_matches.cy.js
+++ cypress/e2e/orbit/get_matches.cy.js
@@ -25,7 +25,7 @@ describe('Extract and Fill Data', () => {
           const competitionText = $competitionLi.text();
 
           if (!competitionText.includes('Challenger') && !competitionText.includes('UTP') && !competitionText.includes('ITF') && (
-            competitionText.includes("WTA Berlin 2025") || competitionText.includes("WTA Nottingham 2025")
+            competitionText.includes("ATP Halle 2025") || competitionText.includes("ATP London 2025")
             )) {
             cy.wrap($competitionLi).click();
 
@@ -64,7 +64,7 @@ describe('Extract and Fill Data', () => {
                           // cy.wait(2000); // 根据需要调整等待时间
 
                           // 处理 event 页面上的数据提取
-                          if (!["34357879","33927225","33953208"].includes(data_event_id))
+                          if (!["34420895"].includes(data_event_id))
                           cy.get(`div[role="row"][data-event-id="${data_event_id}"]`).then(($rowDiv) => {
                             
                             const data_market_id = $rowDiv.attr('data-market-id');
@@ -106,31 +106,24 @@ describe('Extract and Fill Data', () => {
                                   "dash": true,
                                   "vol": 10,
                                   "strategy": {
-                                    "name": "tennis_2",
+                                    "name": "tennis_11",
                                     "params": {
                                       "notInPlay": {
                                         "price": 1.3
                                       },
-                                      "breakdown": {
-                                        "until": 2,
-                                        "side": "BACK",
-                                        "first_runner": true,
-                                        "first_oth": true,
-                                        "profit": 1.0
+                                      "loseHang": {
+                                        "rec": 1.0,
+                                        "set": 3,
+                                        "side": "BACK"
                                       },
-                                      "eitherLose": {
-                                        "first_runner": false,
-                                        "first_oth": false,
+                                      "loseSets": {
+                                        "set": 1,
+                                        "until": 1,
                                         "side": "BACK",
-                                        "until": 1
+                                        "price": 1.7
                                       },
-                                      "eitherDraw": {
-                                        "until": 2,
-                                        "scale": 0
-                                      },
-                                      "drawGames": {
-                                        "side": "BACK",
-                                        "scale": 1
+                                      "drawSets": {
+                                        "side": "BACK"
                                       }
                                     }
                                   },
